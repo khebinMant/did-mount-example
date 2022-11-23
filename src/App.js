82a3ec3424c6d92f8c8437463be3c44d65f39b0e
khabLexander <mantillagka@hotmail.com>
2022-11-23 18:09:24 -0500

    will unmounted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemons: []
+    };
+  }
+
+  //Cuando el componente se desmonta como el return en un useEffet
+  componentWillUnmount() {
+    //Cuando se desmonta no quiero guardar el estado con los pokemos lo limpio
+    this.setState({pokemons:[]})
+  }
+
+  //Cuando el componente se monta como un useEffect
+  componentDidMount() {
+    //Cuando se monta quiero cargar datos de una API
+    this._getPokemons();
+  }
+  render() {
+    return (
+      <>
+      <p>hola</p>
+        {
+          this.state.pokemons.map(pokemon=>(
+            <p key={pokemon.name}>{pokemon.name}</p>
+          ))
+        }
+      </>
+    )
+  }
+
+   async _getPokemons () {
+    //Buscar pokemons de la API de pokemons
+    //componentDidMount es como useEffect pero con react usando clases
+    const resp = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=1')
+    const data = await resp.json();
+    this.setState({pokemons:data.results})
+  }
 }
 
-export default App;
+
+
